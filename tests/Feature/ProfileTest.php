631c8ffaa81b4f5a911bd718ca9f95c1972c2b4d
Fabio Boi <fabio.boi@icloud.com>
2024-04-05 23:50:41 +0200

    Fixed a test to represent soft delete behaviour

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -76,7 +76,9 @@ class ProfileTest extends TestCase
             ->assertRedirect('/');
 
         $this->assertGuest();
-        $this->assertNull($user->fresh());
+
+        // Assert soft deleted
+        $this->assertNotNull($user->refresh()->deleted_at);
     }
 
     public function test_correct_password_must_be_provided_to_delete_account(): void
