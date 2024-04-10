ad70cfc772ffac3e6e3d3f8c5e0609cd027ab0bf
Fabio Boi <fabio.boi@icloud.com>
2024-04-10 17:10:28 +0200

    Hotfixes for enlightn on production

--- app/Console/Commands/ProductionEnlightnCommand.php
+++ app/Console/Commands/ProductionEnlightnCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Enlightn\Enlightn\Enlightn;
 use Illuminate\Console\Command;
 
 class ProductionEnlightnCommand extends Command
@@ -12,14 +13,29 @@ class ProductionEnlightnCommand extends Command
 
     public function handle(): void
     {
-        if(config('enlightn.run_on_production')) {
+        if (config('enlightn.run_on_production')) {
+            // Get all Enlightn test classes
+            $tests = Enlightn::getAnalyzerClasses();
+
+            $shouldNotRun = config('enlightn.dont_run_on_production', []);
+
+            // Filter out tests that should not run on production
+            $tests = array_filter($tests, function ($test) use ($shouldNotRun) {
+                return ! in_array($test, $shouldNotRun);
+            });
+
             $this->info('Running Enlightn on production...');
-            if(config('enlightn.credentials.username') && config('enlightn.credentials.api_token')) {
+            if (config('enlightn.credentials.username', '') !== '' && config('enlightn.credentials.api_token', '') !== '') {
+                echo 'Running with report';
                 $this->call('enlightn', [
-                    '--report'
+                    '--report' => true,
+                    'analyzer' => $tests,
                 ]);
             } else {
-                $this->call('enlightn');
+                $this->call('enlightn', [
+                    $tests,
+                    'analyzer' => $tests,
+                ]);
             }
         }
     }
