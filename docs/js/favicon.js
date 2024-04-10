208837a5f1dbbe0b8d2fcfebb758c0e5873f2dc8
Fabio Boi <fabio.boi@icloud.com>
2024-04-10 02:22:32 +0200

    Removed unused file

--- docs/js/favicon.js
+++
@@ -1,5 +0,0 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    let favicon = document.querySelector('link[rel="icon"]')
-    favicon.href = '/lawtex-favicon-white.png';
-}
-
